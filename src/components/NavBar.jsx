a4378d962659c58350e160a65f4a95aeaae41f9d
Leonowki <zobenza115@gmail.com>
2025-10-02 00:25:01 +0800

    cart cart cart cart cart

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,31 +3,8 @@ import { Link } from "react-router-dom";
 import { Search, ShoppingCart } from "lucide-react";
 import Button from "./button";
 import logo from "../assets/logo.svg";
+import CartDialog from "./Cart";
 
-function CartDialog({ isOpen,onClose}) {
-    if (!isOpen) return null;
-
-    return (
-        <div className="cart-dialog">
-            <div className="cart-header">
-                <div className="flex items-center gap-2">
-                    <h3 className=""> Your Cart</h3>
-                </div>
-                <button onClick={onClose} className="cart-close-btn">✖</button>
-            </div>
-            {/* conditional insert here if adding items next*/}
-            <div>
-                <div className="flex justify-center items-center h-32">
-                    <p className="text-gray-500">Your cart is empty.</p>
-                </div>
-            </div>
-            <div className="flex justify-end">
-                <Button variant="solid" className="mb-2">Checkout</Button>
-            </div>
-        
-        </div>
-    );
-}
 function Navbar() {
     const [searchOpen, setSearchOpen] = useState(false);
     const [query, setQuery] = useState("");
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,8 @@
 
 function Contact() {
     return (
-    <div className = "flex flex-col items-center justify-center w-full  h-100vh bg-[#FFE3E5] bg-transparent">
-            <h1 className="text-4xl font-bold text-[#490101]">
+    <div className = "flex flex-col items-center justify-center w-full  h-100vh bg-transparent ">
+            <h1 className="text-4xl font-bold text-[#490101] ">
                 Contact
             </h1>
             <p className="text-lg text-center text-[#490101]">
